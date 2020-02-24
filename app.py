5b372163322b9292359585fc1ed91ec1a8cc0696
CheesusChrist <32204044+CheesusChrist@users.noreply.github.com>
2020-02-23 21:46:34 -0700

    simple test

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 # app.py
-mport os
+import os
 from flask import Flask
 
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = os.environ['MYSQL_HOST']
-app.config['MYSQL_USER'] = os.environ['MYSQL_USER']
-app.config['MYSQL_PASSWORD'] = os.environ['MYSQL_PASSWORD']
-app.config['MYSQL_DB'] = os.environ['MYSQL_DB']
+#app.config['MYSQL_HOST'] = os.environ['MYSQL_HOST']
+#app.config['MYSQL_USER'] = os.environ['MYSQL_USER']
+#app.config['MYSQL_PASSWORD'] = os.environ['MYSQL_PASSWORD']
+#app.config['MYSQL_DB'] = os.environ['MYSQL_DB']
 
 app.config.from_object(os.environ['APP_SETTINGS'])
 
