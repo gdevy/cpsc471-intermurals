bf9fadae1b5ad47ce91b36122018249057a5fa23
CheesusChrist <32204044+CheesusChrist@users.noreply.github.com>
2020-02-23 22:15:02 -0700

    db test

--- app.py
+++ app.py
@@ -7,10 +7,12 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 
 mysql = MySQL()
-app.config['MYSQL_HOST'] = os.environ['MYSQL_HOST']
-app.config['MYSQL_USER'] = os.environ['MYSQL_USER']
-app.config['MYSQL_PASSWORD'] = os.environ['MYSQL_PASSWORD']
-app.config['MYSQL_DB'] = os.environ['MYSQL_DB']
+
+app.config['MYSQL_DATABASE_USER'] = os.environ['MYSQL_USER']
+app.config['MYSQL_DATABASE_PASSWORD'] = os.environ['MYSQL_PASSWORD']
+app.config['MYSQL_DATABASE_DB'] = os.environ['MYSQL_DB']
+app.config['MYSQL_DATABASE_HOST'] = os.environ['MYSQL_HOST']
+
 mysql.init_app(app)
 
 app.config.from_object(os.environ['APP_SETTINGS'])
@@ -18,20 +20,21 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+	return "Hello World!"
 
 
-@app.route('/test_db')
+@app.route('/test_db/')
 def test_db():
-    conn = mysql.connect()
-    cursor = conn.cursor()
-    cursor.execute("SELECT 1")
-    data = cursor.fetchone()
+	conn = mysql.connect()
+	cursor = conn.cursor()
+	cursor.execute("SELECT 1")
+	data = cursor.fetchone()
+	return "db connection success"
 	
 	
 @app.route('/<name>')
 def hello_name(name):
-    return "Hello {}!".format(name)
+	return "Hello {}!".format(name)
 
 if __name__ == '__main__':
-    app.run()
+	app.run()
