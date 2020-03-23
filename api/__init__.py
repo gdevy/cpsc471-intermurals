f1b24722a8376512fec29dcd42c1f6f0bf91ecec
CheesusChrist <32204044+CheesusChrist@users.noreply.github.com>
2020-03-23 12:21:28 -0600

    added stubs for groupings of endpoints

--- api/__init__.py
+++ api/__init__.py
@@ -13,7 +13,6 @@ def create_app(config_class=Config):
     
     mysql.init_app(app)
     bcrypt.init_app(app)
-    
     from api.teams.routes import teams
     from api.users.routes import users
     # from api.players.routes import players
