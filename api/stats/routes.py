437e91929b90e16d2692048b324ef3fa06ccdabb
josea <janaosea.25@gmail.com>
2020-04-06 18:07:16 -0600

    added preliminary get player stat endpoint

--- api/stats/routes.py
+++ api/stats/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
 from api.models import login_required, User, AccessLevel
+from api import mysql
 
 stats = Blueprint('stats', __name__)
 
@@ -33,3 +34,24 @@ def get_standings(league_id,season_id):
     #return ' '.join(map(str, [row[0] for row in data]))
 	
 	
+@stats.route('/player/', methods=['GET'])
+def get_player_stat():
+	season_id  = request.args.get('seasonID')
+	player_id = request.args.get('playerID')
+	league_id = request.args.get('leagueID')
+
+
+	conn = mysql.connect()
+	cursor = conn.cursor()
+	cursor.callproc('get_player_stat1', args=(season_id, player_id, league_id))
+	data = cursor.fetchone()
+	return jsonify(
+        {
+        'firstName' : data[0],
+        'lastName' : data[1],
+        'points' : float(data[2]),
+        'rebounds' : float(data[3]),
+        'assists' :  float(data[4])
+        }
+    )
+
