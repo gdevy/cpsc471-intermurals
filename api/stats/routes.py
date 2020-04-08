828813095b7f3f63cbcaf5eec3a72a3c42da5605
josea <janaosea.25@gmail.com>
2020-04-08 01:38:13 -0600

    finished get_team_stat endpoint

--- api/stats/routes.py
+++ api/stats/routes.py
@@ -36,18 +36,22 @@ def get_standings(league_id,season_id):
 	
 @stats.route('/player/', methods=['GET'])
 def get_player_stat():
+	# retrieve query string parameters from URL
 	season_id = request.args.get('seasonID', default = None, type = int)
 	player_id = request.args.get('playerID', default = None, type = int)
 	league_id = request.args.get('leagueID',  default = None, type = int)
 
+	# error check: ensure that player_id is provided
 	if not player_id:
 		return jsonify({'message': 'The player_id must be provided'}), 400
 
+	# connect to sql database and call get_player_stat stored procedure
 	conn = mysql.connect()
 	cursor = conn.cursor()
 	cursor.callproc('get_player_stat', args=(season_id, player_id, league_id))
 	data = cursor.fetchall()
 
+	# map procedure output to a list
 	seasons_list = []
 	for i in range(0, len(data)):
 		items = {
@@ -60,6 +64,42 @@ def get_player_stat():
 			'assists':  float(data[i][5])
 		}
 		seasons_list.append(items)
+	
+	# store list into a dictionary
+	seasons_dict = {
+		"seasons" : seasons_list
+	}
+	return jsonify(seasons_dict)
+
+
+@stats.route('/team/', methods=['GET'])
+def get_team_stat():
+	# retrieve query string parameters from URL
+	season_id = request.args.get('seasonID', default = None, type = int)
+	team_id = request.args.get('teamID', default = None, type = int)
+	league_id = request.args.get('leagueID',  default = None, type = int)
+
+	# error check: ensure that player_id is provided
+	if team_id is None:
+		return jsonify({'message': 'The team_id must be provided'}), 400
+
+	# connect to sql database and call get_player_stat stored procedure
+	conn = mysql.connect()
+	cursor = conn.cursor()
+	cursor.callproc('get_team_stat', args=(season_id, team_id, league_id))
+	data = cursor.fetchall()
+
+	# map procedure output to a list
+	seasons_list = []
+	for i in range(0, len(data)):
+		items = {
+			'seasonId': data[i][0],
+			'wins': float(data[i][1]),
+			'losses': float(data[i][2]),
+			'winPercentage': float(data[i][3]),
+		}
+		seasons_list.append(items)
+
 	seasons_dict = {
 		"seasons" : seasons_list
 	}
