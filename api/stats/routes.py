9b2254238368cd85d26695e21438f38ef9c87a97
kmfriedt <kfriedt@gmail.com>
2020-04-11 11:58:20 -0600

    done GET standings route

--- api/stats/routes.py
+++ api/stats/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-from api.models import login_required, User, AccessLevel, Standing
+from api.models import login_required, User, AccessLevel
 from api import mysql
 import json
 stats = Blueprint('stats', __name__)
@@ -24,48 +24,47 @@ def record_game(current_user):
 #public route
 @stats.route('/league/', methods = ['GET'])
 def get_standings():
-    league = request.args.get('league')
-    season = request.args.get('season')
-    
-    #control to make sure we have a league and a season as query parameters.
-    if not season and not league:
-        print('error SEASON and LEAGUE parmeters not provided')
-        return jsonify({'message' : 'season and league parmaeters not provided'}), 400
-    elif not season:
-        print('error SEASON parameter not provided')
-        return jsonify({'message' : 'season parmeter not provided'}), 400
-    elif not league:
-        print('error LEAGUE parameter not privided')
-        return jsonify({'message' : 'league parmeter not provided'}), 400
-    #control to make sure the values are integers? Or is this done by the DB?
-    
-   
-    conn = mysql.connect()
-    cursor = conn.cursor()
-    #try
-    #args = (league_id,season_id)
-    #cursor.callproc('getStandings2, args)
-    
-    cursor.callproc('getStandings2', [league,season])
-    data = cursor.fetchall()
-    print(data)
+	#get values from the query parameters
+	league = request.args.get('league')
+	season = request.args.get('season')
 
-    #These and variations did not work
-        # return jsonify(data)
-        # return ' '.join(map(str, [ row[0] for row in data]))
-        # return jsonify(parse_standings(data))
-        # return json.dumps(parse_standings(data).__dict__)
-    # return '\n'.join(map(str, [row for row in data]))
-    displayData = parse_standings(data)
-    return json.dumps(displayData, default=lambda o: o.__dict__, indent=4)
-    
-    #this one printed them all in line
-        # return '\n'.join(map(str, [json.dumps(row.__dict__) for row in displayData]))
+	#control to make sure both the season and league are passed as query parameters
+	if not season and not league:
+		print('error SEASON and LEAGUE parmeters not provided')
+		return jsonify({'message' : 'season and league parmaeters not provided'}), 400
+	elif not season:
+		print('error SEASON parameter not provided')
+		return jsonify({'message' : 'season parmeter not provided'}), 400
+	elif not league:
+		print('error LEAGUE parameter not privided')
+		return jsonify({'message' : 'league parmeter not provided'}), 400
 	
-def parse_standings(data):
-    result = []
-    for row in data:
-        print(row)
-        print(row[0])
-        result.append(Standing(row[0],row[1],row[2],row[3],row[4]))
-    return result 
+	#control to make sure the values are integers? Or is this done by the DB?
+	conn = mysql.connect()
+	cursor = conn.cursor()
+	cursor.callproc('getStandings2', [league,season])
+	data = cursor.fetchall()
+	
+	#print data to terminal to confirm values
+	print(data)
+	
+	#generate list of results from the database
+	standings_list = []
+	for i in range(0, len(data)):
+		items = {
+			'team_name': data[i][0],
+			'games_played': data[i][1],
+			'wins': data[i][2],
+			'losses': data[i][3],
+			'win_percentage': data[i][4],
+			}
+		standings_list.append(items)
+	
+	#put the list into a dictionary so it can be used by jsonify
+	standings_dict = {
+		"season": season,
+		"league": league,		
+		"standings" : standings_list
+	}
+	
+	return jsonify(standings_dict)
